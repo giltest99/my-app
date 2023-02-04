0f062454be5edc95797fa7cc09b5c21443c52ae7
Gilles <sellig.blater@free.fr>
2023-02-04 23:00:11 +0100

    Minimal app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My APP !</h1>
-      </header>
+      <main>
+        <header className="App-header">
+          <h1>My APP !</h1>
+        </header>
+      </main>
     </div>
   );
 }
